9bd738d069a17fd669c096a81ca51dcf5c034298
zou wendi <542095059@qq.com>
2020-07-14 17:17:48 +0800

    feat: electron依赖处理

---
+++ main.ts
@@ -0,0 +1,54 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    width: 400,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    titleBarStyle: 'hidden',
+    resizable: false,
+    frame: false,
+    show: false,
+    alwaysOnTop: true,
+    transparent: true,
+    hasShadow: true,
+    vibrancy: 'ultra-dark',
+  });
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:3000');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, './dist/index.html'));
+  }
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
