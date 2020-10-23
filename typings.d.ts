0f420f775e781efe2bf09b24c291dc1740693283
zou wendi <542095059@qq.com>
2020-10-23 10:58:12 +0800

    feat: update typeings.d.ts

--- typings.d.ts
+++ typings.d.ts
@@ -1,9 +1,40 @@
-declare module '*.css';
-declare module '*.scss';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.svg';
 declare module '*.json';
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  > & { title?: string }>;
+
+  const src: string;
+  export default src;
+}
+declare module '*.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+declare module '*.less' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare module '@ckeditor/ckeditor5-build-classic';
 declare module '@ckeditor/ckeditor5-build-decoupled-document';
 declare module '@ckeditor/ckeditor5-react';
